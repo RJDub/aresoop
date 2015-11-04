c4e6381fefc164331e5b1367e113da9776e5492a
pauldhein <pauldhein@email.arizona.edu>
2015-11-04 12:47:50 -0700

    Added movement to colonists based off of a point-to-point system

--- src/colonists/Colonist.java
+++ src/colonists/Colonist.java
@@ -1,10 +1,13 @@
 package colonists;
 
+import java.awt.Point;
+
 public abstract class Colonist {
 	Hunger HungerLevel;
 	Thirst ThirstLevel;
 	Fatigue FatigueLevel;
 	int id;
+	Point Location;
 	
 	public Colonist(int input){
 		id = input;
@@ -26,6 +29,14 @@ public abstract class Colonist {
 		return FatigueLevel;
 	}
 	
+	public double getXcoord(){
+		return Location.getX();
+	}
+	
+	public double getYcoord(){
+		return Location.getY();
+	}
+	
 	public boolean isAlive(){
 		if (HungerLevel == Hunger.Dead || ThirstLevel == Thirst.Dead || FatigueLevel == Fatigue.Dead){
 			return false;
@@ -33,4 +44,35 @@ public abstract class Colonist {
 			return true;
 		}
 	}
+	
+	public void moveToDestination(Point dest){
+		double destx = dest.getX();
+		double desty = dest.getY();
+		
+		while (true){
+			if (Location.getX() < destx && Location.getY() < desty){
+				Location.setLocation(Location.getX()+1, Location.getY()+1);
+			} else if (Location.getX() < destx && Location.getY() == desty){
+				Location.setLocation(Location.getX()+1, Location.getY());
+			} else if (Location.getX() < destx && Location.getY() > desty){
+				Location.setLocation(Location.getX()+1, Location.getY()-1);
+			} else if (Location.getX() == destx && Location.getY() < desty){
+				Location.setLocation(Location.getX(), Location.getY()+1);
+			} else if (Location.getX() == destx && Location.getY() == desty){
+				Location.setLocation(Location.getX(), Location.getY());
+			} else if (Location.getX() == destx && Location.getY() > desty){
+				Location.setLocation(Location.getX(), Location.getY()-1);
+			} else if (Location.getX() > destx && Location.getY() < desty){
+				Location.setLocation(Location.getX()-1, Location.getY()+1);
+			} else if (Location.getX() > destx && Location.getY() == desty){
+				Location.setLocation(Location.getX()-1, Location.getY());
+			} else if (Location.getX() > destx && Location.getY() > desty){
+				Location.setLocation(Location.getX()-1, Location.getY()-1);
+			} else {
+				break;
+			}
+		}
+	}
+	
+	
 }
