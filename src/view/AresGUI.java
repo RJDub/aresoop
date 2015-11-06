0a641c047a094cf6e53472cd74a18bb4b1b371eb
pauldhein <pauldhein@email.arizona.edu>
2015-11-06 14:47:10 -0700

    Our UML diagram is amazing

--- src/view/AresGUI.java
+++ src/view/AresGUI.java
@@ -1,7 +1,39 @@
 package view;
 
 import javax.swing.JFrame;
+import management.*;
 
 public class AresGUI extends JFrame{
-
+	
+	private Map board;
+	private Manager build_manager;
+	private Manager col_manager;
+	private Manager goal_manager;
+	private Manager proj_manager;
+	private Manager res_manager;
+	
+	public void main (String[] args){
+		AresGUI view = new AresGUI();
+		view.setVisible(true);
+	}
+	
+	public AresGUI(){
+		layoutGUI();
+		setupModel();
+	}
+	
+	public void layoutGUI(){
+		this.setSize(1200, 1000);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		board = new Map();
+		this.add(board);
+	}
+	
+	public void setupModel(){
+		build_manager = new BuildingManager();
+		col_manager = new ColonistManager();
+		goal_manager = new ColonyGoalManager();
+		proj_manager = new ColonyProjectManager();
+		res_manager = new ResourceManager();
+	}
 }
--- src/view/Map.java
+++ src/view/Map.java
@@ -1,7 +1,25 @@
 package view;
 
+import java.awt.Color;
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
+import tiles.*;
+import colonists.*;
+import buildings.*;
 
 public class Map extends JPanel{
 
+	private Tile[][] BoardModel;
+	private ArrayList<Colonist> colonists;
+	private ArrayList<Building> buildings;
+	public Map(){
+		this.setVisible(true);
+		this.setSize(1000, 1000);
+		this.setBackground(Color.LIGHT_GRAY);
+		
+		BoardModel = new Tile[100][100];
+		colonists = new ArrayList<Colonist>();
+		buildings = new ArrayList<Building>();
+	}
 }
