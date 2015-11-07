9ed11a46fe4c96af54e8e1410702907961a2b19a
Mingcheng Dai <dmcccccc@gmail.com>
2015-11-07 14:02:00 -0700

    group merge try

--- src/view/AresGUI.java
+++ src/view/AresGUI.java
@@ -1,11 +1,20 @@
 package view;
 
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 import management.*;
 
 public class AresGUI extends JFrame{
 	
 	private Map board;
+	private JPanel informationPanel;
+	private JPanel leftPanel;
+	private JScrollPane colonistPanel; // error
+	private JPanel HUD;
 	private Manager build_manager;
 	private Manager col_manager;
 	private Manager goal_manager;
@@ -25,8 +34,14 @@ public class AresGUI extends JFrame{
 	public void layoutGUI(){
 		this.setSize(1200, 1000);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLayout(new GridLayout(2,1));
+		
 		board = new Map();
 		this.add(board);
+		
+		informationPanel = new JPanel();
+		informationPanel.setLayout(new GridLayout(1,3));
+		//informationPanel.add();
 	}
 	
 	public void setupModel(){
