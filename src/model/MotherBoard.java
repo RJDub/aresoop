a2801eb23c5059000768fc79de665cc18f86cc8b
Ryan Wong <ryanwong@email.arizona.edu>
2015-11-07 15:06:59 -0700

    changes to model and removing UML and SEQ dgm's

--- src/model/MotherBoard.java
+++ src/model/MotherBoard.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Vector;
 
 import tiles.*;
 import colonists.*;
@@ -8,14 +10,30 @@ import buildings.*;
 
 
 public class MotherBoard {
-	ArrayList<Tile> tiles = new ArrayList<>();
+	Tile[][] tiles;
 	ArrayList<Colonist> colonists = new ArrayList<>();
 	ArrayList<Building> buldings = new ArrayList<>();
 	
 	public MotherBoard(){
+		tiles = new Tile[10][10];
 		
-		
+		setupBoard();
 	}
 	
-	
+	private void setupBoard(){
+		//random radnom.nextint()
+		Random random = new Random();
+		
+		//assign tiles to each part of the array
+		for (int i = 0; i < 10; i++){
+			for(int j = 0; j<10; j++){
+				int r = random.nextInt(3);
+				switch(r){
+				case 0:
+					tiles[i][j] = new ResourceTile();
+				}
+			}
+		}
+		//resource, ground, obstacle
+	}
 }
