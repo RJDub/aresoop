29f4e6a08c1144d943055646d9a8ef969f6072b0
pauldhein <pauldhein@email.arizona.edu>
2015-11-07 15:14:20 -0700

    I changed some things and stuff

--- src/tiles/ResourceTile.java
+++ src/tiles/ResourceTile.java
@@ -4,30 +4,50 @@ import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class ResourceTile extends Tile{
-	
+public class ResourceTile extends Tile {
+
 	ArrayList<Resource> resources;
+
 	public ResourceTile(Point create) {
 		super(create);
 		// TODO Auto-generated constructor stub
 		resources = new ArrayList<Resource>();
 		determineResources();
 	}
-	
-	private void determineResources(){
+
+	private void determineResources() {
 		Random rando = new Random();
 		Resource res = null;
 		int amount = rando.nextInt(6);
-		for (int i = 0; i < amount; i++){
+		for (int i = 0; i < amount; i++) {
 			int chance = rando.nextInt(7);
-			switch(chance){
+			switch (chance) {
 			case 0:
 				res = Resource.Aquarius;
 				break;
 			case 1:
 				res = Resource.Agrarian;
 				break;
+			case 2:
+				res = Resource.Iron;
+				break;
+			case 3:
+				res = Resource.Nickel;
+				break;
+			case 4:
+				res = Resource.Oxygen;
+				break;
+			case 5:
+				res = Resource.Unobtanium;
+				break;
+			case 6:
+				res = Resource.Thorium;
+				break;
+			default:
+				res = null;
+				break;
 			}
+			resources.add(res);
 		}
 	}
 
