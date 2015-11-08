5db30c83a8435208bd0a2673dd4ffa4e26a79a74
Mingcheng Dai <dmcccccc@gmail.com>
2015-11-07 17:01:30 -0700

    Add several panel inside information panel

--- src/view/AresGUI.java
+++ src/view/AresGUI.java
@@ -1,27 +1,32 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-
-import management.*;
 
 public class AresGUI extends JFrame{
 	
 	private Map board;
 	private JPanel informationPanel;
 	private JPanel leftPanel;
-	private JScrollPane colonistPanel; // error
-	private JPanel HUD;
+	private JPanel middlePanel;
+	private JPanel rightPanel;
+	
+	private ColonistPanel colonist; // error
+	private HUD hud;
+	private BuildingPanel building;
+	
+	/*
 	private Manager build_manager;
 	private Manager col_manager;
 	private Manager goal_manager;
 	private Manager proj_manager;
 	private Manager res_manager;
+	*/
 	
-	public void main (String[] args){
+	public static void main (String[] args){
 		AresGUI view = new AresGUI();
 		view.setVisible(true);
 	}
@@ -34,21 +39,35 @@ public class AresGUI extends JFrame{
 	public void layoutGUI(){
 		this.setSize(1200, 1000);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setLayout(new GridLayout(2,1));
+		this.setLayout(null);
 		
+		// place map on the top of the gui
 		board = new Map();
+		board.setSize(1200, 600);
+		board.setLocation(0, 0);
 		this.add(board);
 		
 		informationPanel = new JPanel();
-		informationPanel.setLayout(new GridLayout(1,3));
-		//informationPanel.add();
+		informationPanel.setLayout(new GridLayout(1,2));
+		colonist = new ColonistPanel();
+		hud = new HUD();
+		building = new BuildingPanel();
+		//informationPanel.add(colonist);
+		//informationPanel.add(hud);
+		//informationPanel.add(building);
+		
+		// place info panel on the bottom of the gui
+		this.add(informationPanel);
+		
+		
 	}
 	
 	public void setupModel(){
-		build_manager = new BuildingManager();
+		/*build_manager = new BuildingManager();
 		col_manager = new ColonistManager();
 		goal_manager = new ColonyGoalManager();
 		proj_manager = new ColonyProjectManager();
 		res_manager = new ResourceManager();
+		*/
 	}
 }
