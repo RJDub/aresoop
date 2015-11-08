41131efb4d2034f8126f06af6b8fe4a005dc7738
pauldhein <pauldhein@email.arizona.edu>
2015-11-08 11:51:41 -0700

    updates to motherboard and resource tile to allow for resource tiles to be determined by random values

--- src/model/MotherBoard.java
+++ src/model/MotherBoard.java
@@ -2,49 +2,74 @@ package model;
 
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Vector;
 
 import tiles.*;
 import colonists.*;
+import enums.Resource;
 import buildings.*;
 
-
 public class MotherBoard {
-	Tile[][] tiles;
-	ArrayList<Colonist> colonists = new ArrayList<>();
-	ArrayList<Building> buldings = new ArrayList<>();
-	
-	public MotherBoard(){
-		tiles = new Tile[10][10];
+	private Tile[][] tiles;
+	private ArrayList<Colonist> colonists;
+	private ArrayList<Building> buldings;
 
-		
+	public MotherBoard() {
+		tiles = new Tile[10][10];
+		colonists = new ArrayList<>();
+		buldings = new ArrayList<>();
 		setupBoard();
 	}
-	
-	private void setupBoard(){
-		//random random.nextint()
+
+	private void setupBoard() {
+		// random random.nextint()
 		Random random = new Random();
-		
-		//assign tiles to each part of the array
-		for (int i = 0; i < 10; i++){
-			for(int j = 0; j<10; j++){
+
+		// assign tiles to each part of the array
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
 				int r = random.nextInt(3);
-				switch(r){
+				switch (r) {
 				case 0:
-					tiles[i][j] = new ResourceTile(new String("./ground.png"));
-				break;
-				
+					tiles[i][j] = new GroundTile(new String("./ground.png"));
+					break;
+
 				case 1:
-					tiles[i][j] = new ResourceTile(new String("./obstacle.png"));
-				break;
-					
+					tiles[i][j] = new ObstacleTile(new String("./obstacle.png"));
+					break;
+
 				case 2:
-					tiles[i][j] = new ResourceTile(new String("./resource.png"));
-				break;
+					int chance = random.nextInt(7);
+					switch (chance) {
+					case 0:
+						tiles[i][j] = new ResourceTile(new String("./resource.png"), Resource.Aquarius);
+						break;
+					case 1:
+						tiles[i][j] = new ResourceTile(new String("./resource.png"), Resource.Agrarian);
+						break;
+					case 2:
+						tiles[i][j] = new ResourceTile(new String("./resource.png"), Resource.Iron);
+						break;
+					case 3:
+						tiles[i][j] = new ResourceTile(new String("./resource.png"), Resource.Nickel);
+						break;
+					case 4:
+						tiles[i][j] = new ResourceTile(new String("./resource.png"), Resource.Oxygen);
+						break;
+					case 5:
+						tiles[i][j] = new ResourceTile(new String("./resource.png"), Resource.Unobtanium);
+						break;
+					case 6:
+						tiles[i][j] = new ResourceTile(new String("./resource.png"), Resource.Thorium);
+						break;
+					default:
+						break;
+					}
+
+					break;
 				}
-				
+
 			}
 		}
-		//resource, ground, obstacle
+		// resource, ground, obstacle
 	}
 }
--- src/tiles/ResourceTile.java
+++ src/tiles/ResourceTile.java
@@ -3,20 +3,25 @@ package tiles;
 import java.util.ArrayList;
 import java.util.Random;
 
+import buildings.ResourceBuilding;
 import enums.Resource;
 import model.*;
 
 public class ResourceTile extends Tile {
 
+	private Resource type;
+	private int capacity;
 	private ArrayList<Resource> resources;
 	private ArrayList<Colonist> occupants;
-	private ArrayList<Building> buildings;
+	private ArrayList<ResourceBuilding> buildings;
 
-	public ResourceTile(String path) {
+	public ResourceTile(String path, Resource in) {
 		super(path);
 		resources = new ArrayList<Resource>();
 		occupants = new ArrayList<Colonist>();
-		buildings = new ArrayList<Building>();
+		buildings = new ArrayList<ResourceBuilding>();
+		capacity = 5;
+		type = in;
 		determineResources();
 	}
 	
@@ -28,51 +33,74 @@ public class ResourceTile extends Tile {
 		return resources;
 	}
 	
-	public ArrayList<Building> getBuildings(){
+	public ArrayList<ResourceBuilding> getBuildings(){
 		return buildings;
+	}
+	
+	private void updateCapacity(){
+		capacity = 5;
+		for (int i = 0; i < buildings.size(); i++){
+			capacity += buildings.get(i).getCapacity();
+		}
 	}
 
 	private void determineResources() {
 		Random rando = new Random();
 		int chance = rando.nextInt(7);
+		updateCapacity();
 		switch (chance) {
 		case 0:
-			for (int i = 0; i < 3; i++){
+			for (int i = 0; i < capacity; i++){
 				resources.add(Resource.Aquarius);
 			}
 			break;
 		case 1:
-			for (int i = 0; i < 3; i++){
+			for (int i = 0; i < capacity; i++){
 				resources.add(Resource.Agrarian);
 			}
 			break;
 		case 2:
-			for (int i = 0; i < 3; i++){
+			for (int i = 0; i < capacity; i++){
 				resources.add(Resource.Iron);
 			}
 			break;
 		case 3:
-			for (int i = 0; i < 3; i++){
+			for (int i = 0; i < capacity; i++){
 				resources.add(Resource.Nickel);
 			}
 			break;
 		case 4:
-			for (int i = 0; i < 3; i++){
+			for (int i = 0; i < capacity; i++){
 				resources.add(Resource.Oxygen);
 			}
 			break;
 		case 5:
-			for (int i = 0; i < 3; i++){
+			for (int i = 0; i < capacity; i++){
 				resources.add(Resource.Unobtanium);
 			}
 			break;
 		case 6:
-			for (int i = 0; i < 3; i++){
+			for (int i = 0; i < capacity; i++){
 				resources.add(Resource.Thorium);
 			}
 			break;
 		default:
 			break;
+		}
+	}
+	
+	public Resource harvest(){
+		for (int i = 0; i < occupants.size(); i++){
+			
+		}
+		return resources.remove(0);
+	}
+	
+	public void generate(){
+		if (resources.size() < capacity){
+			for (int i = resources.size(); i < capacity; i++){
+				resources.add(type);
+			}
 		}
 	}
 	
