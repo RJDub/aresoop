3f4621c7114fccb682333107d433f86f52cf077c
pauldhein <pauldhein@email.arizona.edu>
2015-11-08 11:17:34 -0700

    added Item hirearchy to fulfill usage of items on the spec

---
+++ src/items/SleepingPod.java
@@ -0,0 +1,14 @@
+package items;
+
+import java.awt.Point;
+
+import model.*;
+
+public class SleepingPod extends Item {
+
+	public SleepingPod(Point input) {
+		super(input);
+		// TODO Auto-generated constructor stub
+	}
+
+}
