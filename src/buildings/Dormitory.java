c08f370da7756d73ce3e8aef8ff2d75428454bb4
Mark_Hadley <hadleym@email.arizona.edu>
2015-11-17 12:55:17 -0700

    Added BuildingType enum, which is part of the Building classes.

--- src/buildings/Dormitory.java
+++ src/buildings/Dormitory.java
@@ -1,9 +1,13 @@
 package buildings;
 
+import enums.BuildingType;
 import model.Building;
 
+	 
 public class Dormitory extends Building{
+	public BuildingType buildingType;
 	public Dormitory(){
+		buildingType = BuildingType.Dormitory;
 		super.thirstBonus = 0;
 		super.fatigueBonus = 1000;
 		super.hungerBonus = 0;
--- src/buildings/Mess.java
+++ src/buildings/Mess.java
@@ -1,9 +1,12 @@
 package buildings;
 
+import enums.BuildingType;
 import model.Building;
 
 public class Mess extends Building{
+	public BuildingType buildingType;
 	public Mess(){
+		buildingType = BuildingType.Mess;
 		super.thirstBonus = 0;
 		super.fatigueBonus = 0;
 		super.hungerBonus = 1000;
