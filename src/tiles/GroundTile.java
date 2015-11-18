5e9effb3eaf7bbfef6c0414fae90693fa7bee9d0
Mark_Hadley <hadleym@email.arizona.edu>
2015-11-17 17:44:54 -0700

    Modified tiles.GroundTile(depreciated) to not error.

--- src/tiles/GroundTile.java
+++ src/tiles/GroundTile.java
@@ -12,27 +12,27 @@ public class GroundTile extends Tile {
 
 private ArrayList<Colonist> occupants;
 private ArrayList<Building> buildings;	
-	public GroundTile(){
-		occupants = new ArrayList<Colonist>();
-		buildings = new ArrayList<Building>();
-
-	}
-
-	public ArrayList<Colonist> getOccupants(){
-		return occupants;
-	}
-	
-	public ArrayList<Building> getBuildings(){
-		return buildings;
-	}
-	
-	public Colonist removeOccupant(Colonist person){
-		for (int i = 0; i < occupants.size(); i++){
-			if (occupants.get(i).getName().compareTo(person.getName()) == 0){
-				return occupants.remove(i);
-			}
-		}
-		return null;
-	}
+//	public GroundTile(){
+//		occupants = new ArrayList<Colonist>();
+//		buildings = new ArrayList<Building>();
+//
+//	}
+//
+//	public ArrayList<Colonist> getOccupants(){
+//		return occupants;
+//	}
+//	
+//	public ArrayList<Building> getBuildings(){
+//		return buildings;
+//	}
+//	
+//	public Colonist removeOccupant(Colonist person){
+//		for (int i = 0; i < occupants.size(); i++){
+//			if (occupants.get(i).getName().compareTo(person.getName()) == 0){
+//				return occupants.remove(i);
+//			}
+//		}
+//		return null;
+//	}
 
 }
