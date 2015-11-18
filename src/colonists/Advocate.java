32a3bc66874ee53ece79e8a29784a0d285a74796
Mark_Hadley <hadleym@email.arizona.edu>
2015-11-17 17:48:37 -0700

    Modified the currently depreciated colonists classes so they dont error.

--- src/colonists/Advocate.java
+++ src/colonists/Advocate.java
@@ -3,13 +3,18 @@ package colonists;
 import enums.Advocacy;
 
 public class Advocate extends ProfessionalColonist{
+	public Advocate(String input, int xIn, int yIn) {
+		super(input, xIn, yIn);
+		// TODO Auto-generated constructor stub
+	}
+
 	Advocacy project;
 
-	public Advocate(String input) {
-		super(input);
-		// TODO Auto-generated constructor stub
-		project = Advocacy.None;
-	}
+//	public Advocate(String input) {
+//		super(input);
+//		// TODO Auto-generated constructor stub
+//		project = Advocacy.None;
+//	}
 	
 	public Advocacy getProject(){
 		return project;
--- src/colonists/Worker.java
+++ src/colonists/Worker.java
@@ -4,9 +4,14 @@ import model.Colonist;
 
 public class Worker extends Colonist{
 
-	public Worker(String input) {
-		super(input);
+	public Worker(String input, int xIn, int yIn) {
+		super(input, xIn, yIn);
 		// TODO Auto-generated constructor stub
 	}
+
+//	public Worker(String input) {
+//		super(input);
+//		// TODO Auto-generated constructor stub
+//	}
 
 }
