268400c518bc310f465a7b4448008bb0b390fe1f
Mark_Hadley <hadleym@email.arizona.edu>
2015-11-21 17:00:51 -0700

    added abstract Item.

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,16 +1,13 @@
 package model;
 
-import java.awt.Point;
 
 public abstract class Item {
-
-	private Point location;
 	
-	public Item(Point input){
-		location = input;
+	Colonist owner;
+	
+	public Item(){
+		
 	}
 	
-	public Point getLocation(){
-		return location;
-	}
+	
 }
