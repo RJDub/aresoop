d9668887f43688a5564f74fdc3dbc6cdc6d934fe
Mark_Hadley <hadleym@email.arizona.edu>
2015-11-21 17:20:29 -0700

    Added tests for new Colonist update

--- src/tests/TestColonists.java
+++ src/tests/TestColonists.java
@@ -13,7 +13,19 @@ public class TestColonists {
 	@Test
 	public void test() {
 		Colonist c = new Colonist("George",1,1);
+		assertEquals(1000,c.getFatigueLevel());
+		assertEquals(1000,c.getHungerLevel());
+		assertEquals(1000,c.getThirstLevel());
 		c.update(TileType.Ice);
+		assertEquals(999,c.getFatigueLevel());
+		assertEquals(999,c.getHungerLevel());
+		assertEquals(1000,c.getThirstLevel());
+		c.update(TileType.Crater);
+		assertEquals(998,c.getFatigueLevel());
+		assertEquals(998,c.getHungerLevel());
+		assertEquals(999,c.getThirstLevel());
+		assertEquals(1,c.getX());
+		assertEquals(1,c.getY());
 	}
 
 }
