f59a31779398e36a3bab4790b7494d3fe9f2c38f
Mark_Hadley <hadleym@email.arizona.edu>
2015-11-23 19:56:12 -0700

    Reduced size of Map

--- src/view/AresFrame.java
+++ src/view/AresFrame.java
@@ -42,7 +42,9 @@ public class AresFrame extends JFrame {
 	public static void main(String[] args) {
 
 		ArrayList<Colonist> colonists = new ArrayList<Colonist>();
-		Tile[][] tiles = new Tile[100][100];
+//		Tile[][] tiles = new Tile[100][100];
+		Tile[][] tiles = new Tile[10][10];
+
 //		model = new MotherBoard(colonists, Generator.generateMap(tiles));
 		model = new MotherBoard(colonists, Generator.generateEasyMap(tiles));
 		model.getArrColonists().add(new Colonist("Paul", 0, 0));
