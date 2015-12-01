cabf7e5900a17fc4ec0f4a1f5ddcdb2131afd557
Ryan Wong <ryanwong@email.arizona.edu>
2015-11-30 19:47:07 -0700

    Withdraw methods for water and iron ore in StorageBuilding class. Tests are all still passing.

--- src/tests/TestBuildings.java
+++ src/tests/TestBuildings.java
@@ -13,7 +13,7 @@ import model.MotherBoard;
 public class TestBuildings {
 
 	@Test
-	public void testStorageDesposit() {
+	public void testStorageDespositWithdraw() {
 		StorageBuilding building = new StorageBuilding(50, 50);
 		building.depositResource(3, Task.MiningIce);
 		assertEquals(3, building.getWaterAmount());
@@ -27,9 +27,15 @@ public class TestBuildings {
 		building2.depositResource(1000, Task.MiningIce);
 		assertEquals(1000, building2.getWaterAmount());
 		assertEquals(8, building2.getIronOreAmount());
+
+		building.withdrawWater(2);
+		assertEquals(1, building.getWaterAmount());
 		
+		building2.withdrawIronOre(2);
+		assertEquals(6, building2.getIronOreAmount());
 		
 	}
+	
 	
 	@Test
 	public void testSpawnBuildings(){
