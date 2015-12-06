79837d9ab644785cc73df270f11f274ca82ea929
Mark_Hadley <hadleym@email.arizona.edu>
2015-12-05 23:32:23 -0700

    More constants.

--- src/Helpers/Constants.java
+++ src/Helpers/Constants.java
@@ -2,7 +2,7 @@ package Helpers;
 
 public class Constants {
 	//COLONISTS
-	public static final int COLONIST_START_THIRST = 1;
+	public static final int COLONIST_START_THIRST = 100;
 	public static final int COLONIST_START_HUNGER = 300;
 	public static final int COLONIST_START_FATIGUE = 300;
 	public static final int COLONIST_START_THIRST_THRESHOLD = 100;
@@ -10,8 +10,18 @@ public class Constants {
 	public static final int COLONIST_START_HUNGER_THRESHOLD = 100;
 	public static final int COLONIST_DEFAULT_CAPACITY = 5;
 	
+	//GAME
 	public static final int STARTING_WATER_LEVEL = 100;
-	public static final int STARTING_UNOBTAINIUM_LEVEL = 1001;
+	public static final int STARTING_UNOBTAINIUM_LEVEL = 0;
+	public static final int STARTING_IRON_LEVEL = 50;
+	public static final int STARTING_FOOD_LEVEL = 50;
+	
+	
+	//TILES
+	public static final int DEFAULT_AMOUNT_ORE = 20;
+	public static final int DEFAULT_AMOUNT_MOSSYROCK= 20;
+	public static final int DEFAULT_AMOUNT_ICE= 20;
+	public static final int DEFAULT_AMOUNT_UNOBTAINIUM= 20;
 	
 	
 	
